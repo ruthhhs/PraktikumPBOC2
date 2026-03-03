7aff1cf85d502aef89cedeee2d04ee6afe3a087c
ruthhhs <ruthseptrianas@gmail.com>
2026-03-03 21:46:50 +0700

    garis udah

--- Pertemuan2/src/Garis.java
+++ Pertemuan2/src/Garis.java
@@ -16,14 +16,91 @@ public class Garis {
         counterGaris ++;
     }
 
-    Garis() {
+    Garis() { // garis default
         this(new Titik(0, 0), new Titik(1, 1));
     }
 
     // ====== METHOD SELEKTOR ======
+    Titik getAwal(){
+        return this.awal;
+    }
+
+    Titik getAkhir(){
+        return this.akhir;
+    }
+
+    static int getGounterGaris(){
+        return counterGaris;
+    }
 
     // ====== METHOD MUTATOR ======
+    void setAwal(double x, double y){
+        this.awal.setAbsis(x);
+    }
+
+    void setAkhir(double x, double y){
+        this.akhir.setOrdinat(y);
+    }
 
     // ====== METHOD LAINNYA ======
+    double panjang(){
+        return this.awal.getJarak(this.akhir);
+    }
+
+    double gradien(){
+        return (
+            this.akhir.getOrdinat() - this.awal.getOrdinat() /
+            this.akhir.getAbsis() - this.awal.getAbsis()
+        );
+    }
+
+    Titik getTengah() {
+        return new Titik (
+            (awal.getAbsis() + akhir.getAbsis()) / 2,
+            (awal.getOrdinat() + akhir.getOrdinat()) / 2
+        );
+    }
+
+    void sejajar(Garis G){
+        if (this.gradien() == G.gradien()) {
+            System.out.println("Sejajar");
+        } else {
+            System.out.println("Tidak Sejajar");
+        }
+    }
+
+    void tegakLurus(Garis G){
+        if (this.gradien() * G.gradien() == -1) {
+            System.out.println("Tegak Lurus");
+        } else {
+            System.out.println("Tidak Tegak Lurus");
+        }
+    }
+
+    void printGaris(){
+        System.out.println(
+            "Garis (" + awal.getAbsis() + "," + awal.getOrdinat() + 
+            "), (" + akhir.getAbsis() + "," + akhir.getOrdinat() + ")"
+        );
+    }
+
+    void persamaan(){
+        double m = this.gradien();
+        double c = this.awal.getOrdinat() - (m * this.awal.getAbsis());
+
+        if (c > 0){
+            System.out.println(
+                "Persamaan Garis : y = " + m + "x + " + c
+            );
+        } else if (c < 0){
+            System.out.println(
+                "Persamaan Garis : y = " + m + "x - " + Math.abs(c)
+            );
+        } else {
+            System.out.println(
+                "Persamaan Garis : y = " + m + "x"
+            );
+        }
+    }
 
 } //end class garis
--- Pertemuan2/src/MGaris.java
+++ Pertemuan2/src/MGaris.java
@@ -7,7 +7,41 @@ public class MGaris {
     public static void main(String[] args) {
         Titik T1 = new Titik(1, 2);
         Titik T2 = new Titik(3, 3);
+        Titik T3 = new Titik(2, 2);
+        Titik T4 = new Titik(7, 8);
+
+        Garis G0 = new Garis();
+        G0.printGaris();
+        System.out.println("counterGaris : " + Garis.getGounterGaris());
+        
+        G0.setAwal(2, 3);
+        G0.setAkhir(4, 4);
         Garis G1 = new Garis(T1, T2);
-        Garis G2 = new Garis();
+        Garis G2 = new Garis(T3, T4);
+        
+        G0.printGaris();
+        G1.printGaris();
+        G2.printGaris();
+        System.out.println("counterGaris : " + Garis.getGounterGaris());
+
+        System.out.println(" ");
+
+        System.out.println("Panjang Garis G2 : " + G2.panjang());
+        System.out.println("Gradien Garis G2 : " + G2.gradien());
+        System.out.println("Titik Tengah Garis G2 : ");
+        Titik Tx = G2.getTengah();
+        Tx.printTitik();
+
+        System.out.println(" ");
+
+        System.out.println("Perbandingan G1 dengan G2");
+        G1.sejajar(G2);
+        G1.tegakLurus(G2);
+
+        System.out.println(" ");
+
+        G0.persamaan();
+        G1.persamaan();
+        G2.persamaan();
     }
 }
